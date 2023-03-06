14316a3417e2a762df98188b5ac9a3c387e5d67c
sanjoyk <sanjoy.sardar@hotstar.com>
2023-03-06 23:18:26 +0530

    add /api to each route

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const user = require("./user");
-const item = require("./item");
-const cart = require("./cart");
-const order = require("./order");
+const user = require("./api/user");
+const item = require("./api/item");
+const cart = require("./api/cart");
+const order = require("./api/order");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
@@ -22,16 +22,11 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true })); // to uspport JSON-encoded bodies
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log("sanjoy", req.body);
 
-  next();
-});
-
-app.use("/user", user.router);
-app.use("/item", item.router);
-app.use("/cart", cart.router);
-app.use("/order", order.router);
+app.use("/api/user", user.router);
+app.use("/api/item", item.router);
+app.use("/api/cart", cart.router);
+app.use("/api/order", order.router);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
